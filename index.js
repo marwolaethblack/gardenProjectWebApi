33e036fdfa88c119bbac2224f8531a0d042fc85e
marwolaeth <a.unal677@gmail.com>
2018-02-09 14:02:31 +0100

    add docs

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(compression());
 app.use(bodyParser.json());
 
+app.set("view engine", "ejs"); //Removes the need to put .ejs at the end of ejs files
+app.use(express.static(__dirname + "/public")); //serves css files
+
+
 
 
 app.set('port', process.env.PORT || 3110);
@@ -41,55 +45,82 @@ Light.create({
 })
 */
 
-app.get('/api/lights', (req,res) => {
-	Light.findAll()
-		.then(lights => {
-			res.json(lights);
-		})
-		.catch(errors => {
-			res.json(errors);
-		})
-});
-
-app.post('/api/lights', (req, res) => {
-
-	const body = req.body;
-
-	Light.create({
-		...body
-	})
-	.then(createdLight => {
-		res.json(createdLight);
-	})
-	.catch(err => {
-		res.json(err);
-	})
+app.get('/', (req, res) => {
+	res.redirect('/api');
 })
 
-app.get('/api/plants', (req,res) => {
-	Plant.findAll()
-		.then(plants => {
-			res.json(plants);
-		})
-		.catch(errors => {
-			res.json(errors);
-		})
-});
+app.get('/api', (req, res) => {
+	let routes = [];
+	app._router.stack.forEach((r) => {
+		  if (r.route && r.route.path){
+		    routes.push({
+		    	path: r.route.path,
+		    	methods: r.route.methods
+		    })
+		  } 
+	 }
 
-app.post('api/plants', (req, res) => {
+	)
 
-	const body = req.body;
+	console.log(Plant.attributes);
 
-	Plant.create({
-		...body
-	})
-	.then(createdPlant => {
-		res.json(createdPlant);
-	})
-	.catch(err => {
-		res.json(err);
-	})
+	res.render('./docs/docs', { routes: routes, 
+								plant: {name: "Plant", ...Plant.attributes},
+								light: {name: "Light", ...Light.attributes}
+							})
+
 })
+
+app.route('/api/lights')
+	.get((req,res) => {
+		Light.findAll()
+			.then(lights => {
+				res.json(lights);
+			})
+			.catch(errors => {
+				res.json(errors);
+			})
+	})
+	.post((req, res) => {
+		const body = req.body;
+
+		Light.create({
+			...body
+		})
+		.then(createdLight => {
+			res.json(createdLight);
+		})
+		.catch(err => {
+			res.json(err);
+		})
+	})
+
+
+app.route('/api/plants')
+	.get((req,res) => {
+		Plant.findAll()
+			.then(plants => {
+				res.json(plants);
+			})
+			.catch(errors => {
+				res.json(errors);
+			})
+	})
+	.post((req, res) => {
+
+		const body = req.body;
+
+		Plant.create({
+			...body
+		})
+		.then(createdPlant => {
+			res.json(createdPlant);
+		})
+		.catch(err => {
+			res.json(err);
+		})
+	})
+
 
 app.listen(app.get('port'), () => {
     console.log(`Server is listening on localhost:${app.get('port')}`);
